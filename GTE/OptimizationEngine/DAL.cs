1dcf8ef470ec58a09957bc99a34fb72386ed240a
Golden Ticket Engineering, LLC <info@gldtckeng.com>
2025-05-13 18:40:41 -0700

    25.05.13

---
+++ GTE/OptimizationEngine/DAL.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SQLite;
+
+namespace OptimizationEngine
+{
+    ///
+    internal class DAL
+    {
+        internal DAL()
+        {
+            InitDatabase();
+        }
+
+        private void InitDatabase()
+        {
+            SQLiteConnection conn = new SQLiteConnection("data source = OptimizationEngine.db");
+            try
+            {
+                conn.Open();
+                conn.Close();
+            }
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ GTE/OptimizationEngineTest/DALTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptimizationEngine;
+
+namespace OptimizationEngineTest
+{
+    ///
+    [TestClass]
+    public class DALTest
+    {
+        [TestMethod]
+        public void DALConstructor()
+        {
+            bool ret = false;
+            
+            DAL dal = new DAL();
+            if (File.Exists("OptimizationEngine.db"))
+            {
+                ret &= true;
+            }
+            
+            Assert.IsTrue(ret);
+        }
+    }
+}
